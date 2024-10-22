46bda7c23c37838ab997ed1edefe23bb4989c5f6
Manoj Reddy Poreddy <manojeee251@gmail.com>
2024-10-21 21:06:12 -0400

    Updated launch.py

--- launch.py
+++ launch.py
@@ -1,3 +1,4 @@
+import sys
 from time import sleep
 import nltk
 from selenium import webdriver
@@ -9,48 +10,61 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 import random
 from nltk.corpus import words
 
+def getCount(driver):
+    val_totalRewards = 150
+    count = 5
+    driver.get("https://rewards.bing.com/pointsbreakdown")
+    sleep(10)
+    val_dailyRewards = driver.find_element(By.XPATH, "//a[contains(text(),'PC search')]/../../..//*[@class='pointsDetail c-subheading-3 ng-binding']/b").text
+    print(val_dailyRewards)
+    if( val_totalRewards == val_dailyRewards ):
+        return count
+    else:
+        count = 1
+        print("Executing Search")
+        return count
+
 edge_options = Options()
 # edge_options.add_argument('--headless')
 driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
 # , options=edge_options
 # driver.capabilities("--enable-chrome-browser-cloud-management")
 driver.maximize_window()
-count = 1
-driver.get("https://www.bing.com")
-search_query = "Your search query here"
-sleep(60)
-search_input = driver.find_element(By.NAME, "q")
-search_input.send_keys(search_query)
-search_input.send_keys(Keys.RETURN)
-sleep(15)
-
-nltk.download('words')
-
-# Get the list of English words
-
-dictionary_words = words.words()
-
-# Loop to generate random dictionary words
-for _ in range(40):
-    random_word = random.choice(dictionary_words)
-    print(random_word)
-    icount=driver.find_element(By.ID,"id_rc").text
-    print(icount)
+count = getCount(driver)
+if(count == 1):
+    driver.get("https://www.bing.com")
+    search_query = "Your search query here"
+    sleep(10)
     search_input = driver.find_element(By.NAME, "q")
-    search_input.send_keys(random_word)
+    search_input.send_keys(search_query)
     search_input.send_keys(Keys.RETURN)
     sleep(15)
-    driver.refresh()
-    driver.implicitly_wait(20)
-    rcount2=driver.find_element(By.ID,"id_rc").text
-    print(rcount2)
-    driver.find_element(By.NAME,"q").click()
-    driver.find_element(By.ID,"sw_clx").click()
-    if(count > 5 and icount == rcount2):
-        exit(1)
-        # rcount=driver.find_element(By.ID,"id_rc").text
-    else:
-        print(count)
-        count = count + 1
-# Close the browser
-# driver.quit()
+    nltk.download('words')
+    # Get the list of English words
+    dictionary_words = words.words()
+    # Loop to generate random dictionary words
+    for _ in range(40):
+        random_word = random.choice(dictionary_words)
+        print(random_word)
+        icount=driver.find_element(By.CLASS_NAME,"points-container").text
+        print(icount)
+        search_input = driver.find_element(By.NAME, "q")
+        search_input.send_keys(random_word)
+        search_input.send_keys(Keys.RETURN)
+        sleep(15)
+        driver.refresh()
+        driver.implicitly_wait(20)
+        rcount2=driver.find_element(By.CLASS_NAME,"points-container").text
+        print(rcount2)
+        driver.find_element(By.NAME,"q").click()
+        driver.find_element(By.ID,"sw_clx").click()
+        if(count > 5 and icount == rcount2):
+            sys.exit(1)
+            driver.quit()
+        else:
+            print(count)
+            count = count + 1
+else:
+    print("Search Rewards completed")
+    driver.quit()
+    sys.exit(0)
--- rewards.bat
+++ rewards.bat
@@ -1,24 +1,32 @@
 @echo off
-setlocal
+setlocal EnableDelayedExpansion
 
 REM Set the total number of times to run the command
 set total_runs=15
 
+REM Get the Python home directory
+for /f "delims=" %%i in ('python -c "import sys; print(sys.executable)"') do set PYTHON_HOME=%%i
 
-for /f "delims=" %%i in ('python -c "import sys; print(sys.prefix)"') do set PYTHON_HOME=%%i
-
-REM PYTHON_HOME=python -c "import sys; print(sys.prefix)"
 REM Set the delay in seconds (300 seconds = 5 minutes)
 set delay_seconds=300
 
 REM Loop for the specified number of times
 for /l %%i in (1,1,%total_runs%) do (
     echo Running command, iteration %%i
-    %PYTHON_HOME%/python.exe ./launch.py
+    %PYTHON_HOME% ./launch.py
+    set ret_val=!ERRORLEVEL!
+    echo Python script returned !ret_val!
+
+    REM Handle return value if needed
+    if !ret_val! equ 0 (
+        echo Success!
+        set /a total_runs=15
+    )
 
     REM Wait for the specified delay before the next iteration
+    echo Waiting for %delay_seconds% seconds....
     timeout /nobreak /t %delay_seconds%
 )
 
 echo Batch file completed.
-exit
+exit /b !ret_val!
